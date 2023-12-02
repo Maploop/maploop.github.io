5f1eeedb7f8e8610db15f8c446d0d239940a7bc3
Leon <maploop.val@gmail.com>
2023-12-02 17:53:10 +0330

    Fix

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function reveal() {
       }
     }
   }
+
+  function ScrollToView(elementId)
+  {
+    var element = document.getElementById(elementId);
+    var headerOffset = 200;
+    var elementPosition = element.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+  
+    window.scrollTo({
+         top: offsetPosition,
+         behavior: "smooth"
+    });
+  }
