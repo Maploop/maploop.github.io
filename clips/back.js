2b0f153950d712b1289f225d3c44ab8c6b3d516f
map <76199586+Maploop@users.noreply.github.com>
2024-11-25 13:08:10 +0330

    yay

--- clips/back.js
+++ clips/back.js
@@ -8,7 +8,7 @@ const authToken = urlParams.get("auth");
 if (!clipFormat) clipFormat = "mp4";
 
 // Just display the title of the clip on top
-document.getElementById('clip_title').innerHTML = "Clip: " + clipID;
+document.getElementById("clip_title").innerHTML = "Clip: " + clipID;
 document.title = "§ MeowSite • " + clipID + " in " + clipFormat;
 
 console.log(`Fetching video with ID ${clipID}...`);
@@ -16,32 +16,41 @@ console.log(`Auth token is ${authToken}`);
 
 // Now we fetch the clip from our GitHub repo
 var video_url = "Nothing";
-fetch(`https://api.github.com/repos/refiu/clips/contents/${clipID}.${clipFormat}`, headers={
-    'X-GitHub-Api-Version': '2022-11-28',
-    'Accept': 'application/vnd.github+json',
-    'Authorization': `token ${authToken}`
-}).then(response => response.json()).then(data => {
-    video_url = data['download_url'];
+fetch(
+  `https://api.github.com/repos/refiu/clips/contents/${clipID}.${clipFormat}`,
+  (headers = {
+    "X-GitHub-Api-Version": "2022-11-28",
+    Accept: "application/vnd.github+json",
+    Authorization: `token ${authToken}`,
+  }),
+)
+  .then((response) => response.json())
+  .then((data) => {
+    video_url = data["download_url"];
+    var binaryString = atob(data["content"]);
+    var blob = new Blob([binaryString], { type: `video/${clipFormat}` });
     console.log("Video URL: " + video_url);
 
     // Finally we slot in the video URL
     document.getElementById("clip_player").src = video_url;
-});
+  });
 
 // Website pretty backend lmfao
 document.getElementById("share_button").onclick = (e) => {
-    navigator.clipboard.writeText(window.location.href);
-    document.getElementById("share_button").innerHTML = `<i class="fa-solid fa-check"></i> Copied`;
+  navigator.clipboard.writeText(window.location.href);
+  document.getElementById("share_button").innerHTML =
+    `<i class="fa-solid fa-check"></i> Copied`;
 };
 
 document.getElementById("share_button").onmouseleave = (e) => {
-    document.getElementById("share_button").innerHTML = `<i class="fa-solid fa-share"></i> Share`;
-}
+  document.getElementById("share_button").innerHTML =
+    `<i class="fa-solid fa-share"></i> Share`;
+};
 
 document.getElementById("upload_button").onclick = (e) => {
-    alert("Upload is not yet implemented.");
+  alert("Upload is not yet implemented.");
 };
 
 document.getElementById("download_button").onclick = (e) => {
-    alert("Download is not yet implemented.");
+  alert("Download is not yet implemented.");
 };
